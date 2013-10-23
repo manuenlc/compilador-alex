21ce75e22bf0f1e91d539a79fced88d2b0e94237
manuenlc@gmail.com <manuenlc@gmail.com@e5bf7029-a3c2-ff78-f453-fddaaf1e6a8b>
2013-10-23 03:11:16 +0000

    fill_and_return_token: implementado

--- ProjetoCompilador/lexico.c
+++ ProjetoCompilador/lexico.c
@@ -22,6 +22,46 @@ char nome_lido[ID_TAM_MAX];
 /* funções */
 void print_token(token* to_print);
 void add_char_to_nome_lido(char c);
+token* fill_and_return_token(int token1);
+
+
+token* fill_and_return_token(int token1)
+{
+	token* token_to_return = (token*) malloc(sizeof(token*));
+	if(token_to_return)
+	{
+		printf("erro ao alocar memória");
+		return NULL;
+	}
+
+	token_to_return->token1 = token1;
+
+	switch (token1)
+	{
+		case T_INT_CONST:
+			token_to_return->token_valor_int = atoi(nome_lido);
+			break;
+		case T_BOOLEAN_CONST:
+			if(strcmp("false", nome_lido)) token_to_return->token_valor_boolean = true;
+			else token_to_return->token_valor_boolean = false;
+			break;
+		case T_REAL_CONST:
+			token_to_return->token_valor_real = (float) atof(nome_lido);
+			break;
+		case T_ID:
+			strcpy(token_to_return->token_valor_id, nome_lido);
+			token_to_return->token2 = get_token2(token_to_return->token_valor_id);
+			break;
+		case T_EOF:
+			strcpy(token_to_return->token_valor_id, "eof");
+			break;
+		default:
+			strcpy(token_to_return->token_valor_id, nome_lido);
+			break;
+	}
+	return token_to_return;
+}
+
 
 void add_char_to_nome_lido(char c)
 {
