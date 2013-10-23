0d6914f03a3932851cbbfded77f784eb204b39c9
manuenlc@gmail.com <manuenlc@gmail.com@e5bf7029-a3c2-ff78-f453-fddaaf1e6a8b>
2013-10-23 03:01:50 +0000

    print_token implementado

--- ProjetoCompilador/lexico.c
+++ ProjetoCompilador/lexico.c
@@ -10,13 +10,50 @@
 #include <ctype.h>
 #include <string.h>
 
+#include "tokens.h"
+#include "boolean.h"
+
+
+/* funções */
+void print_token(token* to_print);
+
+
+void print_token(token* to_print)
+{
+	if(to_print == NULL)
+	{
+		printf("token NULL");
+		return;
+	}
+
+	printf("%d\t\t", to_print->token1);
+
+	switch (to_print->token1)
+	{
+		case T_INT_CONST:
+			printf(" \t\t%d", to_print->token_valor_int);
+			break;
+		case T_BOOLEAN_CONST:
+			if(to_print->token_valor_boolean) printf(" \t\ttrue");
+			else printf(" \t\tfalse");
+			break;
+		case T_REAL_CONST:
+			printf(" \t\t%f", to_print->token_valor_real);
+			break;
+		case T_ID:
+			printf("%d\t\t", to_print->token2);
+			printf("%s", to_print->token_valor_id);
+			break;
+		default:
+			printf("%s", to_print->token_valor_id);
+			break;
+	}
+
+		printf("\n");
+}
+
 token* next_token()
 {
 	printf("TODO");
 	return NULL;
-}
-
-void print_token(token* to_print)
-{
-	printf("TODO");
 }
--- ProjetoCompilador/main.c
+++ ProjetoCompilador/main.c
@@ -8,9 +8,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "tokens.h"
+#include "boolean.h"
+#include "lexico.h"
 
 int main()
 {
-	printf("hello world!");
+	token* tk = (token*) malloc(sizeof(token*));
+	if(!tk) {printf("erro ao alocar memoria"); return -1;}
+
+	tk->token1 = T_BOOLEAN_CONST;
+	tk->token2 = 10;
+	tk->token_valor_int = 20;
+	tk->token_valor_real = 13.2;
+	tk->token_valor_boolean = false;
+	strcpy(tk->token_valor_id, "hello");
+
+	print_token(tk);
+
+	free(tk);
+
 	return 0;
 }
